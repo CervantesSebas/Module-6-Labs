d6936013dc4ebfb916662ba668a9bd3f6c8780b3
Sebastian Cervantes <scervantes3@my.nl.edu>
2024-10-29 15:59:18 -0500

    Add files via upload

---
+++ randomday.py
@@ -0,0 +1,8 @@
+#Sebastian Cervantes 
+#October 29, 2024 
+#Selects a day of the week and prints message 
+import random 
+week = {"Monday", "Tuesday", "Wednesday","Thursday","Friday"}
+day = random.choice(["Monday", "Tuesday", "Wednesday","Thursday","Friday"])
+
+print("It was a rainy", day,"and the lightning flashed across the sky")
